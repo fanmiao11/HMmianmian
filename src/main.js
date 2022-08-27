9179423ada3095897242e7ad37bd68c0c8462b18
fanmiao11 <miao623@outlook.com>
2022-08-27 10:19:49 +0800

    打包优化

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@
 import Vue from 'vue'
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 import Element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+// import 'element-ui/lib/theme-chalk/index.css'
 import '@/styles/index.scss' // global css
 import App from './App'
 import router from './router'
@@ -19,7 +19,7 @@ import i18n from './lang' // Internationalization
 import './icons' // icon
 import './errorLog' // error log
 import * as filters from './filters' // global filters
-import './mock' // simulation data
+// import './mock' // simulation data
 // font-awesome
 import "font-awesome/css/font-awesome.css";
 /*
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,34 @@
-const path = require('path')
+const path = require("path");
+
+let externals = {};
+if (process.env.NODE_ENV === "production") {
+  externals = {
+    echarts: "echarts",
+    "element-ui": "ELEMENT",
+    vue: "Vue",
+    xlsx: "XLSX",
+    "cos-js-sdk-v5": "COS",
+  };
+}
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
 module.exports = {
   publicPath: "./",
   lintOnSave: false,
+
+  configureWebpack: {
+    // webpack排除打包
+    externals,
+  },
+
   chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].myEnv = process.env.NODE_ENV;
+      return args;
+    });
+
     const svgRule = config.module.rule("svg");
     svgRule.uses.clear();
     svgRule
@@ -22,5 +48,45 @@ module.exports = {
       .end()
       .use("file-loader")
       .loader("file-loader");
+
+    config.when(process.env.NODE_ENV !== "development", (config) => {
+      config
+        .plugin("ScriptExtHtmlWebpackPlugin")
+        .after("html")
+        .use("script-ext-html-webpack-plugin", [
+          {
+            // `runtime` must same as runtimeChunk name. default is `runtime`
+            inline: /runtime\..*\.js$/,
+          },
+        ])
+        .end();
+      // 提取公共代码
+      config.optimization.splitChunks({
+        chunks: "all",
+        cacheGroups: {
+          libs: {
+            name: "chunk-libs",
+            test: /[\\/]node_modules[\\/]/,
+            priority: 10,
+            chunks: "initial", // only package third parties that are initially dependent
+          },
+          elementUI: {
+            name: "chunk-elementUI", // split elementUI into a single package
+            priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+            test: /[\\/]node_modules[\\/]_?element-ui(.*)/, // in order to adapt to cnpm
+          },
+          commons: {
+            name: "chunk-commons",
+            test: resolve("src/components"), // can customize your rules
+            minChunks: 3, //  minimum common number
+            priority: 5,
+            reuseExistingChunk: true,
+          },
+        },
+      });
+      // https:// webpack.js.org/configuration/optimization/#optimizationruntimechunk
+      // 运行时代码
+      config.optimization.runtimeChunk("single");
+    });
   },
 };
